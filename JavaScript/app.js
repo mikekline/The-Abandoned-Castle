c9c7dd58420f5ebf412a4d6a5bd4667fa2964194
Michael Kline <klinem@gmail.com>
2020-04-14 04:55:46 -0600

    rooms done

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,78 +1,101 @@
-import * as rooms from './rooms.js';
+import * as data from './data.js';
 
-let room = "";
 let currentRoom = "road";
 
+
+/*********************************************/
+/*          room navigation               */
+/*********************************************/
 function selectRoom (e) {
   switch (currentRoom){
+    /*on the road*/
     case 'road':
-      if (e == "n" || e.keyCode === 78 ){ 
-        document.getElementById('article').innerHTML = rooms.gate.description; 
-        currentRoom = 'Front Gate';
-      }
+        if (e == "n" || e.keyCode === 78 || e.keyCode === 38){ 
+            document.getElementById('description').innerText = data.gate.description;
+            document.getElementById('direction').innerText = data.gate.directions;
+            currentRoom = 'Front Gate';
+          }
       break;
-
+    
+    /*Front Gate*/
     case 'Front Gate':
-      if (e == "n" || e.keyCode === 78 ){ 
-        document.getElementById('article').innerHTML = rooms.courtYard.description;
+      if (e == "n" || e.keyCode === 78 || e.keyCode === 38){ 
+        document.getElementById('description').innerText = data.courtYard.description;
+        document.getElementById('direction').innerText = data.courtYard.directions;
         currentRoom = 'Courtyard';
       }
       break;
   
+    /*Courtyard*/
     case 'Courtyard':
-      if (e == "n" || e.keyCode === 78 ){ 
-        document.getElementById('article').innerHTML = rooms.banquet.description;
+      if (e == "n" || e.keyCode === 78 || e.keyCode === 38){ 
+        document.getElementById('description').innerText = data.banquet.description;
+        document.getElementById('direction').innerText = data.banquet.directions;
         currentRoom = 'Banquet Room';
-      } else if (e == "e" || e.keyCode === 69 ){ 
-        document.getElementById('article').innerHTML = rooms.dungeon.description;
+      } else if (e == "e" || e.keyCode === 69 || e.keyCode === 39){ 
+        document.getElementById('description').innerText = data.dungeon.description;
+        document.getElementById('direction').innerText = data.dungeon.directions;
         currentRoom = 'Dungeon';
-      } else if (e == "s" || e.keyCode === 83 ){ 
-        document.getElementById('article').innerHTML = `<p>"The gates have sealed behind you, 
-                                             you can no longer go that way."</p>` + rooms.courtYard.directions;
+      } else if (e == "s" || e.keyCode === 83 || e.keyCode === 40){ 
+        document.getElementById('description').innerText = "The gates have sealed behind you. You can no longer go that way.";
+        document.getElementById('direction').innerText = "You can go north, east, west";
         currentRoom = 'Courtyard';
-      } else if (e == "w" || e.keyCode === 87 ){ 
-        document.getElementById('article').innerHTML = rooms.stables.description;
+      } else if (e == "w" || e.keyCode === 87 || e.keyCode === 37){ 
+        document.getElementById('description').innerText = data.stables.description;
+        document.getElementById('direction').innerText = data.stables.directions;
         currentRoom = 'Stables';
       }
       break;
  
+    /*Stables*/  
     case 'Stables':
-      if (e == "e" || e.keyCode === 69 ){ 
-        document.getElementById('article').innerHTML = rooms.courtYard.description;
+      if (e == "e" || e.keyCode === 69 || e.keyCode === 39){ 
+        document.getElementById('description').innerText = data.courtYard.description;
+        document.getElementById('direction').innerText = data.courtYard.directions;
         currentRoom = 'Courtyard';
       }
       break;
  
+    /*Banquet Room*/ 
     case 'Banquet Room':
-      if (e == "n" || e.keyCode === 78 ){ 
-        room = document.getElementById('article').innerHTML = rooms.kitchen.description;
+      if (e == "n" || e.keyCode === 78 || e.keyCode === 38){ 
+        document.getElementById('description').innerText = data.kitchen.description;
+        document.getElementById('direction').innerText = data.kitchen.directions;
         currentRoom = 'Kitchen';
-      } else if (e == "s" || e.keyCode === 83 ){ 
-        document.getElementById('article').innerHTML = rooms.courtYard.description;
+      } else if (e == "s" || e.keyCode === 83 || e.keyCode === 40){ 
+        document.getElementById('description').innerText = data.courtYard.description;
+        document.getElementById('direction').innerText = data.courtYard.directions;
         currentRoom = 'Courtyard';
       }
       break;
  
+    /*Kitchen*/
     case 'Kitchen':
-      if (e == "s" || e.keyCode === 83 ){ 
-        document.getElementById('article').innerHTML = rooms.banquet.description;
+      if (e == "s" || e.keyCode === 83 || e.keyCode === 40){ 
+        document.getElementById('description').innerText = data.banquet.description;
+        document.getElementById('direction').innerText = data.banquet.directions;
         currentRoom = 'Banquet Room';
       }
       break;
- 
+
+    /*Dungeon*/
     case 'Dungeon':
-      if (e == "w" || e.keyCode === 87 ){ 
-        document.getElementById('article').innerHTML = rooms.courtYard.description;
+      if (e == "w" || e.keyCode === 87 || e.keyCode === 37){ 
+        document.getElementById('description').innerText = data.courtYard.description;
+        document.getElementById('direction').innerText = data.courtYard.directions;
         currentRoom = 'Courtyard';
-      } else if (e == "e" || e.keyCode === 69 ){ 
-        document.getElementById('article').innerHTML = rooms.cave.description;
+      } else if (e == "e" || e.keyCode === 69 || e.keyCode === 39){ 
+        document.getElementById('description').innerText = data.cave.description;
+        document.getElementById('direction').innerText = data.cave.directions;
         currentRoom = 'Cave';
       }
       break;
  
+    /*Cave*/
     case 'Cave':
-      if (e == "w" || e.keyCode === 87 ){ 
-        document.getElementById('article').innerHTML = rooms.dungeon.description;
+      if (e == "w" || e.keyCode === 87 || e.keyCode === 37){ 
+        document.getElementById('description').innerText = data.dungeon.description;
+        document.getElementById('direction').innerText = data.dungeon.directions;
         currentRoom = 'Dungeon';
       }
       break;
@@ -83,44 +106,15 @@ function selectRoom (e) {
   }  
 }
  
+
+
+
+/*********************************************/
+/*          input controllers                */
+/*********************************************/
+
 document.addEventListener('keydown', selectRoom);
 document.querySelector('#north').onclick = function(){selectRoom('n')};
 document.querySelector('#east').onclick = function(){selectRoom('e')};
 document.querySelector('#south').onclick = function(){selectRoom('s')};
 document.querySelector('#west').onclick = function(){selectRoom('w')};
-
-
-
-// let room = document.getElementById('article');
-    // room.innerHTML = rooms.courtYard.description;
-    // console.log(rooms.courtYard.description);
-
-
-
-//you are lost, find your way back to the light
-
-
-
-
-
-// // let roomName = rooms.gate.name
-// const north = document.querySelector('#north');
-
-//  function handleKeyPress (e) {
-//     console.log("woot")
-//     // document.querySelector('.north').onclick = "null";
-//     // if (roomName === courtYard)
-//     if (e || e.keyCode === 13) {
-
-//       let room = document.getElementById('article');
-//       room.innerHTML = rooms.courtYard.description;
-//         roomName = rooms.courtYard.name;
-//       console.log(roomName);
-//       return false;
-//     }
-//     return false;
-//   }
-//   north.addEventListener('click', () => handleKeyPress);
-// document.addEventListener('keydown', handleKeyPress);
-
-
